b75fb1c51ba2a5a0a720936a102c118764530c9f
cristian-boaventura <96512124+cristian-boaventura@users.noreply.github.com>
2022-09-02 18:30:58 -0300

    using reselect library to create memoized selectors

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,6 +1,20 @@
-export const selectCategoriesMap = (state) =>
-  state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  // input array: if its values don't change (strict equal), the output function won't run
+  [selectCategoryReducer],
+  // output function: its paramaters are each one of the inputs in the array.
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) =>
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+);
