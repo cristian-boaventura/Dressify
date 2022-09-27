30fb0a2c42fa6eea0544e12147e9fe7e0cf02ff5
cristian-boaventura <96512124+cristian-boaventura@users.noreply.github.com>
2022-09-27 15:41:50 -0300

    typed cart reducer

---
+++ src/store/cart/cart.selector.ts
@@ -0,0 +1,31 @@
+import { createSelector } from "reselect";
+
+import { CartState } from "./cart.reducer";
+
+const selectCartReducer = (state: any): CartState => {
+  return state.cart;
+};
+
+export const selectCartItems = createSelector([selectCartReducer], (cart) => {
+  return cart.cartItems;
+});
+
+export const selectIsCartOpen = createSelector(
+  [selectCartReducer],
+  (cart) => cart.isCartOpen
+);
+
+export const selectCartCount = createSelector(
+  [selectCartItems],
+  (cartItems): number =>
+    cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
+);
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  (cartItems): number =>
+    cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity * cartItem.price,
+      0
+    )
+);
